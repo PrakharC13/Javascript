f7530ca0a7905c4728003eaafecca598d8e1f098
Prakhar Chaudhary <prakharchaudhary13@gmail.com>
2024-12-14 00:33:47 +0000

    variable

---
+++ basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountemail="prakharchaudhary13@gmail.com"
+var accountpassword="jaipur"
+accountcity="Goa"
+let accountstate; //if value is not given to a variable then the value is undefined
+
+//accountId=2//not allowed as its constant 
+/* let is used insted of var because of issue in block scope and functional scope */
+accoutEmail="mrpc31yt@gmail.com"
+accountpassword="323eed"
+accountcity="Porvorim"
+console.log(accountId);
+console.table([accountemail,accountpassword,accountstate,accountcity])
+
+
